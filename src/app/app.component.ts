bbaf39427b2b43746ab3263154bcad1f3e29d365
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-14 10:11:45 +0100

    chuck norris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
+import {Joke} from "./dto/Joke";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -11,4 +14,30 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chucknorris';
+  value: String | undefined
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    interval(3000).subscribe(() => {
+      this.apiCall()
+      });
+    }
+
+  apiCall() {
+      this.getAPI().subscribe(
+        (response) => {
+          this.value = response.value;
+        },
+        (error) => {
+          console.log("Error");
+        }
+      );
+
+  }
+
+  getAPI () {
+    return this.http.get<Joke>('https://api.chucknorris.io/jokes/random')
+  }
 }
